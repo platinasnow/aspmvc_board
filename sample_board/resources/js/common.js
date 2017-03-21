a17d8a709eae47bfefeb0f4d391029b52c6da9ad
인상학 <platinasnow@naver.com>
2017-03-21 13:56:13 +0900

    검색 기능 추가

---
+++ sample_board/resources/js/common.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $('.search_text').keyup(function (e) {
+        if (e.keyCode == 13) {
+            searchList();
+        }
+    });
+});
+function searchList() {
+    $('#page').val(1);
+    $('form').submit();
+};
